c1a602ba5a752c3865a0bc9369fbda85737202eb
GalaxyUGC <coronelpalma.christian@gmail.com>
2025-01-30 11:51:34 -0500

    CAMBIOENRUTAS

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -2,15 +2,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/login'
 import Header from './components/header'
+import MainContent from './components/mainContent'
 
 function App() {
   return (
     <>
+    <Header/>
       <Routes>
-        <Route path='./components/header.tsx' element={<Header/>}/>
-        <Route path='./components/login.tsx' element={<Login/>}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/dashboard' element={<MainContent/>}/>
       </Routes>
-      <Login/>  
     </>
   )
 }
--- Dashboard/src/components/mainContent.tsx
+++ Dashboard/src/components/mainContent.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Sidebar from './sidebar';
 
 const MainContent: React.FC = () => {
     return (
+        <>
+        <Sidebar/>
         <main className="main-content">
             <div className="progress-bar">
                 <div className="progress" style={{ width: '57%' }}></div>
             </div>
             <span>57%</span>
         </main>
+        </>
     );
 };
 
