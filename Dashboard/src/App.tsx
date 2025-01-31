77dd6f7bf39cbc0a6078864bdd4c267ecdd5c7e3
GalaxyUGC <coronelpalma.christian@gmail.com>
2025-01-30 20:50:16 -0500

    updateDash

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -1,13 +1,12 @@
 
 import { Route, Routes } from 'react-router-dom'
 import Login from './components/login'
-import Header from './components/header'
 import MainContent from './components/mainContent'
+import './styles/dashboard.css'
 
 function App() {
   return (
     <>
-    <Header/>
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/dashboard' element={<MainContent/>}/>
--- Dashboard/src/components/mainContent.tsx
+++ Dashboard/src/components/mainContent.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import Header from './header';
 import Sidebar from './sidebar';
 
-const MainContent: React.FC = () => {
+function mainContent() {
+    
     return (
         <>
-        <Sidebar/>
-        <main className="main-content">
-            <div className="progress-bar">
-                <div className="progress" style={{ width: '57%' }}></div>
-            </div>
-            <span>57%</span>
-        </main>
-        </>
+        
+            <Header/>
+            <Sidebar/>
+            <main className="main-content">
+                <div className="progress-bar">
+                    <div className="progress" style={{ width: '57%' }}></div>
+                </div>
+                <span>57%</span>
+            </main>
+            </>
     );
 };
 
-export default MainContent;
+export default mainContent;
